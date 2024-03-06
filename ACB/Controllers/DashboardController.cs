15fa7d998c21dfd523510af5c4b2eee439cb4bd7
Teggenberg <tdeggenberger@student.fullsail.edu>
2024-03-06 14:13:29 -0600

    sidebar model added

--- ACB/Controllers/DashboardController.cs
+++ ACB/Controllers/DashboardController.cs
@@ -1,17 +1,48 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ACB.Areas.Identity.Data;
+using ACB.Data;
+using ACB.Models;
+//using Microsoft.AspNet.Identity;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using System.Data;
 
 namespace ACB.Controllers
 {
     public class DashboardController : Controller
     {
+        private readonly ACBContext _context;
+        private readonly UserManager<ACBUser> _userManager;
+        private readonly SignInManager<ACBUser> _signInManager;
+
+        public DashboardController(UserManager<ACBUser> userManager,
+            SignInManager<ACBUser> signInManager)
+        {
+            this._userManager = userManager;
+            this._signInManager = signInManager;
+        }
+
         public IActionResult Index()
         {
             return View();
         }
 
-        public IActionResult Home()
+        public IActionResult SideBar( ContractorVM contracotr) 
         {
-            return View();
+            var currentUser = _userManager.GetUserName;
+
+            var user = currentUser.ToString();
+            Query.GetContractor(user);
+            return PartialView("../Shared/_sidebar", contracotr);
+        }
+
+        public IActionResult Home(ContractorVM contractor)
+        {
+            if(contractor == null)
+            {
+                contractor = new ContractorVM();
+            }
+            SideBar(contractor);
+            return View(contractor);
         }
 
 
--- ACB/Models/ContractorVM.cs
+++ ACB/Models/ContractorVM.cs
@@ -6,8 +6,16 @@
         public string? FirstName { get; set; }
         public string? LastName { get; set;}
         public string? Email { get; set; }
-        public List<int> Services { get; set; }
-        public List<QuoteVM> Quotes { get; set; }
+        public List<int>? Services { get; set; }
+        public List<QuoteVM>? Quotes { get; set; }
+
+        public ContractorVM() 
+        { 
+
+            Quotes = new List<QuoteVM>();   
+            Services = new List<int>();
+        
+        }
 
     }
 }
