6901520df2ef79099fb219f4a91749d556c92a0b
Teggenberg <tdeggenberger@student.fullsail.edu>
2024-03-17 11:14:19 -0500

    branching to work on quote date

--- ACB/Controllers/Query.cs
+++ ACB/Controllers/Query.cs
@@ -173,6 +173,9 @@ namespace ACB.Controllers
                     Firstname = (string?)dt.Rows[i][1],
                     Lastname = (string?)dt.Rows[i][2],
                     Email = (string?)dt.Rows[i][3],
+                    Zip = Convert.ToInt32(dt.Rows[i][5]),
+                    //Address = (string?)dt.Rows[i][6],
+                    Details = (string?)dt.Rows[i][7],
                     Service = (string?)dt.Rows[i][12]
                 };
                 quotes.Add(quote);
