edf1776bd63e4d8432988e305fe3a18a4f3ba7d4
Morgen Pence <MAPence@student.fullsail.edu>
2024-03-19 12:33:25 -0500

    find contractor 80% complete

---
+++ ACB/Controllers/LocationController.cs
@@ -0,0 +1,43 @@
+﻿using ACB.Data;
+using ACB.Models;
+using Microsoft.AspNetCore.Mvc;
+using NuGet.ProjectModel;
+
+
+
+namespace ACB.Controllers
+{
+    public class LocationController : Controller
+    {
+
+        private readonly ACBContext _context;
+
+        public LocationController(ACBContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet]
+        public IActionResult Location()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index(Location location)
+        {
+            if (ModelState.IsValid)
+            {
+                return RedirectToAction("Location");
+            }
+
+            return View("../Location/Location");
+        }
+
+        public IActionResult Confirmation()
+        {
+            return View();
+        }
+    }
+
+}
