c5f1b4da8ca5ae70f0999b59a407b768cce45572
Morgen Pence <MAPence@student.fullsail.edu>
2024-03-19 20:07:34 -0500

    commits

--- ACB/Models/Quote.cs
+++ ACB/Models/Quote.cs
@@ -5,17 +5,16 @@ namespace ACB.Models
     public class Quote
     {
         public int id { get; set; }
-        public string? client_first_name { get; set; }
-        public string? client_last_name { get; set; }
-        public string? client_email { get; set; }
-        public DateTime? created { get; set; }
-        public string? details { get; set; }
-        public string? address { get; set; }
-        [Range(10000, 99999)] // Causes number input
-        public string zip { get; set; }
+        public string? Client_first_name { get; set; }
+        public string? Client_last_name { get; set; }
+        public string? Client_email { get; set; }
+        public DateTime? Created { get; set; }
+        public string? Details { get; set; }
+        public string? Address { get; set; }
+        public string? Zip { get; set; }
 
-        public string? city { get; set; }
-        public string? state { get; set; }
-        public int? service { get; set; }
+        public string? City { get; set; }
+        public string? State { get; set; }
+        public int? Service { get; set; }
     }
 }
