ff24a3d11913ac2b0a0269ed0e492410e9503acb
Teggenberg <tdeggenberger@student.fullsail.edu>
2024-03-25 20:46:36 -0500

    fixed quote query

--- ACB/Controllers/Query.cs
+++ ACB/Controllers/Query.cs
@@ -191,7 +191,7 @@ namespace ACB.Controllers
                     Zip = Convert.ToInt32(dt.Rows[i][5]),
                     //Address = (string?)dt.Rows[i][6],
                     Details = (string?)dt.Rows[i][7],
-                    Service = (string?)dt.Rows[i][14]
+                    Service = (string?)dt.Rows[i][16]
                 };
                 quotes.Add(quote);
             }
