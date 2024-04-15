1f07a4ae648b4e0e0408fd3d32dd757b3a1440a1
Teggenberg <tdeggenberger@student.fullsail.edu>
2024-04-15 10:36:20 -0500

    cleaned up job form and backend logic

--- ACB/Models/JobVM.cs
+++ ACB/Models/JobVM.cs
@@ -8,6 +8,7 @@ namespace ACB.Models
         public string? Firstname { get; set; }
         public string? Lastname { get; set; }
         public string? Email { get; set; }
+        public string? Phone { get; set; }
         public string? Service { get; set; }
         public string? Address { get; set; }
         public string? City { get; set; }
